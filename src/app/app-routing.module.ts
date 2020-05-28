0673b985c53b2ce99b4369a85c9e659abb0fc6e7
Papa Thiam <papathiame11@gmail.com>
2020-05-28 08:39:31 +0000

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -44,6 +44,10 @@ const routes: Routes = [
     path: 'profil',
     loadChildren: () => import('./profil/profil.module').then( m => m.ProfilPageModule)
   },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  }
 ];
 
 @NgModule({
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -14,8 +14,9 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
-      //{path: 'tabs', loadChildren: () => import('../tabs/tabs.module').then( m => m.TabsPageModule)},
+      {path: 'home', loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)},
       {path: 'profil', loadChildren: () => import('../profil/profil.module').then( m => m.ProfilPageModule)},
+      {path: 'search', loadChildren: () => import('../search/search.module').then( m => m.SearchPageModule)},
     ]
   },
   {
